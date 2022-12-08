7704cc156f5f1e9fa3fbd44ce80ae04b69b14c90
dakshveer0091 <108885356+dakshveer0091@users.noreply.github.com>
2022-12-08 19:50:04 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent('sphere',{
+    schema:{
+        moveY:{type:"number",default:1}
+
+    },
+
+    tick:function(){
+        this.data.moveY+=0.1
+        var pos=this.el.getAttribute("position")
+        pos.Y=this.data.moveY
+        this.el.setAttribute("position",{x:pos.x,y:pos.y,z:pos.z})
+    },
+
+})
